95652ad6c5d1e4e96b0bc4b97afafabd461ee0b0
Dmitry Medvinsky <dmedvinsky@gmail.com>
2011-06-28 01:14:00 +0400

    Show hours/minutes usage
    Workrave records the times you start using the computer and you stop to.
    Statrave calculates the difference.

--- src/main.hs
+++ src/main.hs
@@ -1,37 +1,46 @@
 module Main (main) where
 
-import System.Locale
-import Data.Time
+import Data.Time (readTime, UTCTime, NominalDiffTime, diffUTCTime)
+import System.Locale (defaultTimeLocale)
 
 
-main = interact processStat
+main = interact processInput
 
 
-processStat :: String -> String
-processStat = buildResult . parse . filter f . lines
+processInput :: String -> String
+processInput = buildResult . parse . filter f . lines
     where f = (==) 'D' . head
 
 
 parse :: [String] -> [(UTCTime, UTCTime)]
 parse []     = []
-parse (x:xs) = let both  = drop 1 $ words x
-                   start = unwords $ fix $ take 5 both
-                   end   = unwords $ fix $ drop 5 both
-                   startTime = parseOne start
-                   endTime   = parseOne end
-                in (startTime, endTime) : parse xs
-    where parseOne :: String -> UTCTime
-          parseOne = readTime defaultTimeLocale "%d %m %Y %H %M"
-          fix (d:m:y:h:i:[]) = (pad d)
+parse (x:xs) = let dates = drop 1 $ words x
+                   start = parseUglyDate $ take 5 dates
+                   end   = parseUglyDate $ drop 5 dates
+                in (start, end) : parse xs
+
+parseUglyDate :: [String] -> UTCTime
+parseUglyDate = readTime defaultTimeLocale "%d %m %Y %H %M" . unwords . fix
+    where fix (d:m:y:h:i:[]) = (pad d)
                              : (pad $ show $ read m + 1)
                              : (show $ read y + 1900)
                              : (pad h)
                              : (pad i)
                              : []
-          pad :: String -> String
-          pad x = if length(x) == 1 then "0" ++ x else x
 
+pad :: String -> String
+pad x = if length(x) == 1 then '0' : x else x
+
+minutes :: NominalDiffTime -> Int
+minutes = truncate . (/ 60) . realToFrac
+
+hours :: NominalDiffTime -> Int
+hours = truncate . (/ 60) . fromIntegral . minutes
 
 buildResult :: [(UTCTime, UTCTime)] -> String
 buildResult = unlines . map f
-    where f (s, e) = show s ++ " — " ++ show e
+    where f (s, e) = let date = head $ words $ show s
+                         diff = e `diffUTCTime` s
+                         hrs = hours diff
+                         mins = (minutes diff) - hrs * 60
+                      in date ++ " — " ++ pad (show hrs) ++ ":" ++ pad (show mins)
